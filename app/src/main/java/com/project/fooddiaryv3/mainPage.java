ff368c6681bf27b8bc1829357bfb4236bfbb7554
TerranceChu <a0321456821@gmail.com>
2024-06-26 16:53:11 +0800

    success to make the diary sample template

--- app/src/main/java/com/project/fooddiaryv3/mainPage.java
+++ app/src/main/java/com/project/fooddiaryv3/mainPage.java
@@ -3,13 +3,30 @@ package com.project.fooddiaryv3;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class mainPage extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private DiaryAdapter diaryAdapter;
+    private List<DiaryEntry> diaryEntries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        diaryEntries = new ArrayList<>();
+        diaryEntries.add(new DiaryEntry("First Entry", "This is the content of the first entry.", "2024-06-26"));
+        diaryEntries.add(new DiaryEntry("Second Entry", "This is the content of the second entry.", "2024-06-27"));
+
+        diaryAdapter = new DiaryAdapter(diaryEntries);
+        recyclerView.setAdapter(diaryAdapter);
     }
 }
