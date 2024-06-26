fc7ca5d3b3463505c7d849ded220e3051becf976
TerranceChu <a0321456821@gmail.com>
2024-06-26 16:48:50 +0800

    create diary datetype diaryEntry

---
+++ app/src/main/java/com/project/fooddiaryv3/mainPage.java
@@ -0,0 +1,15 @@
+package com.project.fooddiaryv3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+
+    }
+}
