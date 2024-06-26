6281032bc8b8b2a227bbfe1f54aff77ebf79467c
TerranceChu <a0321456821@gmail.com>
2024-06-26 16:51:01 +0800

    create diary DiaryAdapter

---
+++ app/src/main/java/com/project/fooddiaryv3/DiaryAdapter.java
@@ -0,0 +1,50 @@
+package com.project.fooddiaryv3;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class DiaryAdapter extends RecyclerView.Adapter<DiaryAdapter.DiaryViewHolder> {
+    private List<DiaryEntry> diaryEntries;
+
+    public DiaryAdapter(List<DiaryEntry> diaryEntries) {
+        this.diaryEntries = diaryEntries;
+    }
+
+    @NonNull
+    @Override
+    public DiaryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.diary_entry_item, parent, false);
+        return new DiaryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DiaryViewHolder holder, int position) {
+        DiaryEntry diaryEntry = diaryEntries.get(position);
+        holder.titleTextView.setText(diaryEntry.getTitle());
+        holder.contentTextView.setText(diaryEntry.getContent());
+        holder.dateTextView.setText(diaryEntry.getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return diaryEntries.size();
+    }
+
+    public static class DiaryViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTextView, contentTextView, dateTextView;
+
+        public DiaryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.titleTextView);
+            contentTextView = itemView.findViewById(R.id.contentTextView);
+            dateTextView = itemView.findViewById(R.id.dateTextView);
+        }
+    }
+}
